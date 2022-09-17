1f9865241f31ea20a80471a7cec23cd8b9aa18e1
Omar Ali AbdelSalam <excalibur1987@gmail.com>
2022-09-17 21:58:02 +0300

    :tada: first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<main>
+  <header>
+    <span>Note App</span>
+  </header>
+</main>
+
+<style lang="scss">
+  header {
+    height: 7vh;
+    background-color: #7ae99b;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
